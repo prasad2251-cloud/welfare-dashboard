db90daf1b04246081776ffce08d55fb261f611ae
prasad2251-cloud <prasad2251@gmail.com>
2026-06-17 09:50:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,41 +2,40 @@ document.addEventListener("DOMContentLoaded", loadData);
 
 async function loadData() {
 
-    const api =
-    "https://script.google.com/macros/s/AKfycbz-wDK6Z-WhCr6yOvUtSGca6vVtInsdYBsi4_PgF-XOAjqS8_91fxhwh3dIAhzx_upy/exec";
+  const api = "PASTE_YOUR_GOOGLE_APPS_SCRIPT_URL_HERE";
 
-    const response = await fetch(api);
-    const data = await response.json();
+  const response = await fetch(api);
+  const data = await response.json();
 
-    // Total employees count
-    document.getElementById("total").innerText = data.length;
+  document.getElementById("total").innerText = data.length;
 
-    let html = "<table>";
+  document.getElementById("birthday").innerText = 0;
+  document.getElementById("marriage").innerText = 0;
 
+  let html = `
+  <table>
+  <tr>
+    <th>Police Station</th>
+    <th>Rank</th>
+    <th>Employee Name</th>
+    <th>Date of Birth</th>
+    <th>Mobile No</th>
+  </tr>
+  `;
+
+  data.forEach(emp => {
     html += `
     <tr>
-        <th>Police Station</th>
-        <th>Rank</th>
-        <th>Employee Name</th>
-        <th>Date of Birth</th>
-        <th>Mobile No</th>
+      <td>${emp.ps}</td>
+      <td>${emp.rank}</td>
+      <td>${emp.employee}</td>
+      <td>${emp.dob}</td>
+      <td>${emp.mobile}</td>
     </tr>
     `;
+  });
 
-    data.forEach(emp => {
+  html += "</table>";
 
-        html += `
-        <tr>
-            <td>${emp.ps}</td>
-            <td>${emp.rank}</td>
-            <td>${emp.employee}</td>
-            <td>${emp.dob}</td>
-            <td>${emp.mobile}</td>
-        </tr>
-        `;
-    });
-
-    html += "</table>";
-
-    document.body.innerHTML += html;
+  document.getElementById("tableData").innerHTML = html;
 }
