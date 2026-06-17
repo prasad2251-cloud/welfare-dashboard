7de246cc3183eaf6b31266d6a511e1986c72452d
prasad2251-cloud <prasad2251@gmail.com>
2026-06-17 18:44:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 document.addEventListener("DOMContentLoaded", loadData);
 
 async function loadData() {
+  alert("Script Running");
+
   const api = "https://script.google.com/macros/s/AKfycbzu9mnQb_VIvkNZCRN6uFcTW9_7NYuG9kdzO-QuROl9awOFUGq5gUXvS5TxL9IsRa19yA/exec";
 
   try {
     const response = await fetch(api);
     const data = await response.json();
 
-    console.log(data);
+    alert("Records = " + data.length);
 
     document.getElementById("total").innerText = data.length;
 
   } catch (error) {
+    alert("Error: " + error);
     console.log(error);
-    alert(error);
   }
 }
