d9bd6818cfbc1a062eb949252b764fdadab64300
prasad2251-cloud <prasad2251@gmail.com>
2026-06-17 08:47:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", loadData);
 
-    alert("Chittoor Police Welfare Dashboard Loaded Successfully");
+async function loadData() {
 
-});
+  const api =
+  "https://script.google.com/macros/s/AKfycbz-wDK6Z-WhCr6yOvUtSGca6vVtInsdYBsi4_PgF-XOAjqS8_91fxhwh3dIAhzx_upy/exec";
+
+  const response = await fetch(api);
+  const data = await response.json();
+
+  let html = "<h2>Employee Welfare Data</h2>";
+
+  html += "<table border='1' cellpadding='10' style='margin:auto; border-collapse:collapse;'>";
+
+  html += "<tr>";
+  html += "<th>Police Station</th>";
+  html += "<th>Rank</th>";
+  html += "<th>Employee Name</th>";
+  html += "<th>Date of Birth</th>";
+  html += "<th>Mobile No</th>";
+  html += "</tr>";
+
+  data.forEach(emp => {
+
+      html += "<tr>";
+
+      html += "<td>" + emp.ps + "</td>";
+
+      html += "<td>" + emp.rank + "</td>";
+
+      html += "<td>" + emp.employee + "</td>";
+
+      html += "<td>" + emp.dob + "</td>";
+
+      html += "<td>" + emp.mobile + "</td>";
+
+      html += "</tr>";
+
+  });
+
+  html += "</table>";
+
+  document.body.innerHTML =
+      "<h1>CHITTOOR POLICE WELFARE DASHBOARD</h1>" + html;
+}
